5d1cec4ca61a7308e0a1bc48404129a4a1670aa4
UseR <highermath80@gmail.com>
2022-03-24 12:40:01 +0600

    Class 4

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class,'index']);
+Route::get('/redirects', [HomeController::class,'redirects']);
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
